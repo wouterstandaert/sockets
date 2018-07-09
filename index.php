2123fd36092658e11591c24286b9fe1842be1da1
Wouter Standaert <Wouter@MacBook-Pro-van-Wouter.local>
2018-07-09 21:14:00 +0200

    Enabled basic templating

--- index.php
+++ index.php
@@ -2,15 +2,25 @@
 
 require_once './vendor/autoload.php';
 
+// Load template directories
 $loader = new Twig_Loader_Filesystem(
     array(
-        __DIR__ . '/src/assets/html',
+        __DIR__ . '/src/assets/templates',
         __DIR__ . '/src/modules/dashboard/views',
     )
 );
 
+// Instantiate a new twig environment with some default settings
 $twig = new Twig_Environment($loader, [
-    'cache' => __DIR__ . '/cache/views'
+    //'cache' => __DIR__ . '/cache/views',
+    'debug' => true
 ]);
 
-echo $twig->render('dashboard.twig');
+// Enable debug mode
+$twig->addExtension(new Twig_Extension_Debug());
+
+// Render the main template
+$layout = $twig->render('layout.twig');
+
+// Render the page template
+echo $twig->render('dashboard.twig', array('Content' > $layout));
