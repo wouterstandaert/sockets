c47867d6a2269288fc4a911a35eb7fd6803579d2
Wouter Standaert <WouterS@webatvantage.local>
2018-07-10 13:52:59 +0200

    Sent user data onopen

--- src/modules/dashboard/js/dashboard.js
+++ src/modules/dashboard/js/dashboard.js
@@ -1,12 +1,15 @@
 // Establish connection
 var socket = new WebSocket('ws://localhost:8080');
 
-socket.send(JSON.stringify({
-    event : 'joined',
-    data : {
-        user : {
-            id : 1,
-            name : 'John'
+socket.onopen = function(e) {
+    socket.send(JSON.stringify({
+        event : 'joined',
+        data : {
+            user : {
+                id : 1,
+                name : 'John'
+            }
         }
-    }
-}));
+    }));
+
+};
