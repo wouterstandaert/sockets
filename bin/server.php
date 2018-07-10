524034c56e089595e8167562fdff43822ed57311
Wouter Standaert <WouterS@webatvantage.local>
2018-07-10 12:23:37 +0200

    Renamed base namespace

--- bin/server.php
+++ bin/server.php
@@ -3,7 +3,7 @@
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
-use MyApp\Intranet;
+use App\Intranet;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
--- src/intranet.php
+++ src/intranet.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MyApp;
+namespace App;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
