46ae097bb2dd61c422b76c9ed77f3fb4ebb26761
Wouter Standaert <WouterS@webatvantage.local>
2018-07-10 12:57:16 +0200

    Added join event

--- src/modules/dashboard/js/dashboard.js
+++ src/modules/dashboard/js/dashboard.js
@@ -1,14 +1,12 @@
 // Establish connection
-var conn = new WebSocket('ws://localhost:8080');
+var socket = new WebSocket('ws://localhost:8080');
 
-// Handle on open functionality
-conn.onopen = function(e) {
-    console.log("Connection established!");
-};
-
-// Handle incoming messages
-conn.onmessage = function(event) {
-    console.log(event.data);
-
-    $('#active-users').find('tbody').append('<tr><td>' + event.data + '</td><td>Online</td></tr>');
-};
+socket.send(JSON.stringify({
+    event : 'joined',
+    data : {
+        user : {
+            id : 1,
+            name : 'John'
+        }
+    }
+}));
