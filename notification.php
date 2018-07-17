8376799dd59fa4d37a4ba775fae3ea05fce5ab4c
Wouter Standaert <Wouter@MacBook-Pro-van-Wouter.local>
2018-07-17 22:42:43 +0200

    Code refactoring

--- notification.php
+++ notification.php
@@ -6,5 +6,4 @@ require_once './vendor/autoload.php';
 $context = new \ZMQContext();
 $socket = $context->getSocket(\ZMQ::SOCKET_PUSH);
 $socket->connect('tcp://127.0.0.1:5555');
-
 $socket->send('Hello world');
