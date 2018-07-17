68666a3adab1325de964def88dbee411fc477c20
Wouter Standaert <Wouter@MacBook-Pro-van-Wouter.local>
2018-07-17 21:53:30 +0200

    Implemented basic client

--- src/pusher.php
+++ src/pusher.php
@@ -2,9 +2,30 @@
 
 namespace App;
 
+use React\EventLoop\LoopInterface;
+use React\ZMQ\SocketWrapper;
 use Thruway\Peer\Client;
 
 class Pusher extends Client
 {
-    
+    protected $socket;
+
+
+    public function __construct($realm, LoopInterface $loop, SocketWrapper $socket)
+    {
+        parent::__construct($realm, $loop);
+        $this->socket = $socket;
+    }
+
+
+    public function onSessionStart($session, $transport)
+    {
+        $this->socket->on('message', [$this, 'transmit']);
+    }
+
+
+    public function transmit($payload)
+    {
+        $this->getSession()->publish('chat', ['Hello word']);
+    }
 }
