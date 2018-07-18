46b7a2c576df960c753b494750497b30d98a7ae9
Wouter Standaert <Wouter@MacBook-Pro-van-Wouter.local>
2018-07-18 21:35:08 +0200

    Implemented a static handler class that submits push messages

---
+++ src/engine/pushhandler.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+class PushHandler
+{
+    /**
+     * The active socket connection
+     *
+     * @var \ZMQSocket
+     */
+    private static $socket;
+
+
+    /**
+     * Initialize our push controller
+     */
+    private static function init()
+    {
+        if (!isset(self::$socket))
+        {
+            $context = new \ZMQContext();
+
+            self::$socket = $context->getSocket(\ZMQ::SOCKET_PUSH);
+
+            self::$socket->connect('tcp://127.0.0.1:5555');
+        }
+    }
+
+
+    /**
+     * Send some content to our socket
+     *
+     * @param mixed $payload The content you want to the websocket
+     */
+    public static function send($payload)
+    {
+        self::getSocket()->send($payload);
+    }
+
+
+    /**
+     * Get the active socket
+     *
+     * @return \ZMQSocket
+     */
+    private static function getSocket()
+    {
+        self::init();
+
+        return self::$socket;
+    }
+}
