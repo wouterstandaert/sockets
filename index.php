c044c54944f65f8580b8f668c38f034c1191ba6d
Wouter Standaert <Wouter@MacBook-Pro-van-Wouter.local>
2018-07-18 21:16:47 +0200

    Implemented some test data

--- index.php
+++ index.php
@@ -5,9 +5,26 @@ require_once './vendor/autoload.php';
 
 // Create Router instance
 $router = new \Bramus\Router\Router();
+// Set default namespace
+$router->setNamespace('\App\Controllers');
+
+// Handle authentication
+$router->before('GET', '/.*', function () {
+    // @todo: fix authentication
+});
 
 // Display the dashboard
-$router->get('dashboard', '\App\Controllers\DashboardController@showDashboard');
+$router->get('dashboard', 'DashboardController@showDashboard');
+
+$router->mount('/users', function() use ($router) {
+
+    // Get a list of users
+    $router->get('/', 'UserController@getUsers');
+
+    // Get a single user
+    $router->get('/(\d+)', 'UserController@getUser');
+
+});
 
 // Set a fallback
 $router->set404('\App\Controllers\DashboardController@showDashboard');
--- src/modules/users/usercontroller.php
+++ src/modules/users/usercontroller.php
@@ -25,11 +25,34 @@ class UserController extends BaseController
     public function getUsers()
     {
         // @todo: Finish up users functionality
+
+        $users = [
+            [
+                'firstname' => 'Alain',
+                'lastname' => 'Vandam'
+            ],
+            [
+                'firstname' => 'Frankie',
+                'lastname' => 'Loosveld'
+            ]
+        ];
+
+        header('Content-Type: application/json');
+        echo json_encode($users);
+        exit();
     }
 
 
-    public function getUser()
+    public function getUser($userID)
     {
-        // @todo: Finish up user functionality
+        // @todo: Make this dynamic
+        $user = [
+            'firstname' => 'John',
+            'lastname' => 'Doe'
+        ];
+
+        header('Content-Type: application/json');
+        echo json_encode($user);
+        exit();
     }
 }
