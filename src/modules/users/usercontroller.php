531540b828412cd8f2024f0ccecd51e229abe6b8
Wouter Standaert <Wouter@MacBook-Pro-van-Wouter.local>
2018-07-18 21:03:55 +0200

    Added basic user controller

---
+++ src/modules/users/usercontroller.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+class UserController extends BaseController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function signIn()
+    {
+        // @todo: Finish up sign in functionality
+    }
+
+
+    public function signOut()
+    {
+        // @todo: Finish up sign out functionality
+    }
+
+
+    public function getUsers()
+    {
+        // @todo: Finish up users functionality
+    }
+
+
+    public function getUser()
+    {
+        // @todo: Finish up user functionality
+    }
+}
